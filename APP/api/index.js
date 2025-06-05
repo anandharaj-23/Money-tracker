9485a0038802331674c122fe322066f07b4021aa
anandharaj-23 <142414242+anandharaj-23@users.noreply.github.com>
2025-06-05 23:37:47 +0530

    Update index.js

--- APP/api/index.js
+++ APP/api/index.js
@@ -19,7 +19,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 /*
 app.get('/api/test', (req,res) => {
